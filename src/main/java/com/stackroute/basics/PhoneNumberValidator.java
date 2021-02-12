9bfacff8bcc6e8106e3f2d915522d2783e76f961
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-02-12 12:58:16 +0530

    completed

--- src/main/java/com/stackroute/basics/PhoneNumberValidator.java
+++ src/main/java/com/stackroute/basics/PhoneNumberValidator.java
@@ -1,24 +1,57 @@
 package com.stackroute.basics;
 
+import java.util.Scanner;
+
 public class PhoneNumberValidator {
     //Create Scanner object as instance variable
+    private Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
         //call the functions in the required sequence
+        PhoneNumberValidator phNumVal = new PhoneNumberValidator();
+        phNumVal.displayResult(
+                phNumVal.validatePhoneNumber(phNumVal.getInput())
+        );
+
     }
 
     public String getInput() {
-        return null;
+        return scn.nextLine();
     }
 
     public void displayResult(int result) {
-        //displays the result
+        if (result == 1) {
+            System.out.println("Valid");
+        }else {
+            System.out.println("Invalid");
+        }
     }
 
     public int validatePhoneNumber(String input) {
+        int countDigits = 0;
+
+        for(char ch: input.toCharArray()){
+            if(ch=='-'){
+                continue;
+            }
+            else {
+                if(ch=='0'||ch=='1'||ch=='2'||ch=='3'||ch=='4'||ch=='5'||ch=='6'||ch=='7'||ch=='8'||ch=='9'){
+                    countDigits++;
+                }
+                else{
+                    return 0;
+                }
+            }
+        }
+
+        if(countDigits==10){
+            return 1;
+        }
         return 0;
+
     }
-    public void closeScanner(){
-        //close the Scanner object
+
+    public void closeScanner() {
+        scn.close();
     }
 }
